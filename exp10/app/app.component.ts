5ccfeea4585a358864b079f77f6627d47d234b6c
Aravinth S <80613214+ambiaravinth@users.noreply.github.com>
2021-11-19 13:24:17 +0530

    Add files via upload

---
+++ exp10/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'AravinthS109';
+  txt = '19itl109';
+
+}
